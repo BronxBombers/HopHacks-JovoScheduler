4e05b752bc1e07ce0e6cf7c7196dc47fe66f6af0
Zach Morgan <zachmorgan987221@gmail.com>
2018-09-15 06:49:11 -0400

    adfs

--- app/app.js
+++ app/app.js
@@ -22,6 +22,23 @@ app.setHandler({
         this.toIntent('HelloWorldIntent');
     },
 
+    'BookAllInfoProvided' : function(
+        appointmentType, business, 
+        location, date, time) {
+        let customerName = this.AlexaUser().getName();
+        let customerEmail = this.AlexaUser().getEmail();
+        let customerPhone = this.AlexaUser().getMobileNumber();
+        let businessData = dynamo_read(business, location);
+        let bookingAPIPayload = 
+        `{
+            "start_datetime":"",
+            "service":"${appointmentType}",
+            "customer_email":"${customerEmail}",
+
+        }`
+        this.ask("hello", "reprompt ");
+    },
+
     'HelloWorldIntent': function() {
         this.ask('Hello World! What\'s your name?', 'Please tell me your name.');
     },
@@ -30,5 +47,10 @@ app.setHandler({
         this.tell('Hey ' + name.value + ', nice to meet you!');
     },
 });
+
+function formatDate(date, hour) {
+    var dateString = date + "T";
+    let abbreviationStrings = []
+}
 
 module.exports.app = app;
